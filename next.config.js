a121d01dea5b39a2554db95c162c4e3b6011f090
SRIRAM <24211a05q9@bvrit.ac.in>
2026-02-01 07:00:00 +0000

    Unhandled Runtime Error TypeError: app.registerVersion is not a function

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  transpilePackages: ['firebase', '@firebase/app', '@firebase/auth', '@firebase/firestore', '@firebase/storage'],
   images: {
     remotePatterns: [
       {
