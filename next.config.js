3811508a52333475b3bbbfd0d98fb0b4f4de7385
SRIRAM <24211a05q9@bvrit.ac.in>
2026-02-01 05:57:23 +0000

    it shows these:Build Failed Command "npm run build" exited with 1

--- next.config.js
+++ next.config.js
@@ -23,6 +23,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      /Critical dependency: require function is used in a way in which dependencies cannot be statically extracted/,
+    ];
+    return config;
+  },
 };
 
 module.exports = nextConfig;
